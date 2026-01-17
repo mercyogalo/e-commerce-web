c366f7f10a42f7d484a741cb97cf7d4ac5475ccd
Mercy Ogalo <ogalomercy8@gmail.com>
2026-01-18 00:17:17 +0300

    Added the get products router logic

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,19 @@
 const express=require("express");
 const router=express.Router();
 const Product=require("../models/Product");
+const protect=require("../middleware/auth");
+
+
+router.get('products',async (req,res)=>{
+    try {
+        const products=await Product.find();
+
+        if(products.length===0){
+            res.status(201).json({message:"Currently their are no products"});
+        }
+
+        res.status(200).json(products);
+    } catch (error) {
+        return res.status(500).json({message:"Server error"});
+    }
+});
